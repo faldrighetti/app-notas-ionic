80f71980dbf631d359728493b88e72ae2d95568e
Federico Aldrighetti <f.a.aldrighetti@gmail.com>
2023-12-26 02:20:47 -0300

    Crea funciones de agregar, editar y eliminar
    Resta integrar con Firebase y guardar las notas al recargar la página.

--- AppNotas/src/app/home/home.page.ts
+++ AppNotas/src/app/home/home.page.ts
@@ -1,36 +1,87 @@
-import { CommonModule } from '@angular/common';
-import { Component, ViewChild } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { IonModal, IonicModule } from '@ionic/angular';
+import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { IonModal } from '@ionic/angular';
 import { OverlayEventDetail } from '@ionic/core/components';
+import { Note, NoteService } from '../services/notes/note.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  styleUrls: ['home.page.scss']
 })
-export class HomePage {
+export class HomePage implements OnInit, OnDestroy {
 
-  constructor() {}
+  constructor(private note: NoteService){}
 
-  @ViewChild(IonModal) modal!: IonModal;
+  @ViewChild(IonModal) ionModal!: IonModal;
+  noteSubscription!: Subscription;
   model: any = {};
+  notesArray: Note[] = [];
+  isOpen: boolean = false;
 
-  message = 'This modal example uses triggers to automatically open a modal when the button is clicked.';
-  name!: string;
-
-  cancel() {
-    this.modal.dismiss(null, 'cancel');
-  }
-
-  confirm() {
-    this.modal.dismiss(this.name, 'confirm');
+  ngOnInit(): void {
+    this.note.getNote()
+    this.noteSubscription = this.note.notes.subscribe({
+      next: (notes) => {
+        this.notesArray = notes;
+      },
+      error: (e) => {
+        console.log(e);
+      }
+    })
   }
 
   onWillDismiss(event: Event) {
     const ev = event as CustomEvent<OverlayEventDetail<string>>;
-    if (ev.detail.role === 'confirm') {
-      this.message = `Hello, ${ev.detail.data}!`;
+    this.model = {};
+    this.isOpen = false;
+  }
+
+  cancel() {
+    this.ionModal.dismiss(null, 'cancel');
+  }
+
+  async save(form: NgForm) {
+    try {
+      if(!form.value.title || !form.value.description){
+        //Llamar a un ion-alert
+        return;
+      }
+      else {
+        console.log(form.value);
+        if(this.model.id){
+          await this.note.editNote(this.model.id, form.value);
+        } else{
+          await this.note.addNote(form.value);
+        }
+        this.ionModal.dismiss();
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async editNote(note: Note){
+    try {
+      this.isOpen = true;
+      this.model = {...note}
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async deleteNote(note: Note){
+    try {
+      await this.note.deleteNote(note?.id!);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  ngOnDestroy(): void {
+    if(this.noteSubscription){
+      this.noteSubscription.unsubscribe();
     }
   }
 }
--- AppNotas/src/app/services/notes/note.service.ts
+++ AppNotas/src/app/services/notes/note.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 
-interface Note {
+export interface Note {
   title: string;
   description: string;
+  id?: string;
 }
 
 @Injectable({
@@ -20,7 +21,12 @@ export class NoteService {
 
   async addNote(data: Note){
     try {
-      
+      const currentNotes = this.$notes.value;
+      let notesArray: Note[] = [{...data, id: (currentNotes.length + 1).toString()}]; //Es un array de Note (título + descripción) que desestructura lo que hay en data y lo asigna allí.
+      notesArray = notesArray.concat(currentNotes);
+      this.$notes.next(notesArray); //Se emite el valor del array actualizado al observable $notes.
+      return notesArray; //Devuelve el array
+
     } catch (error) {
       throw(error);
     }
@@ -28,7 +34,7 @@ export class NoteService {
 
   async getNote(){
     try {
-      
+      return [];
     } catch (error) {
       throw(error);
     }
@@ -37,7 +43,10 @@ export class NoteService {
 
   async editNote(id: string, data: Note){
     try {
-      
+      const currentNotes = this.$notes.value;
+      const index = currentNotes.findIndex(element => element.id == id); //Busca el elemento cuyo id coincide con el del parámetro
+      currentNotes[index] = {id, ...data};
+      this.$notes.next(currentNotes); //Lleva todo lo que hay en currentNotes al observable
     } catch (error) {
       throw(error);
     }
@@ -46,7 +55,9 @@ export class NoteService {
 
   async deleteNote(id: string){
     try {
-      
+      let currentNotes = this.$notes.value;
+      currentNotes = currentNotes.filter(element => element.id != id); //Crea un array con los elementos que cumplen la condición del filter
+      this.$notes.next(currentNotes);
     } catch (error) {
       throw(error);
     }
